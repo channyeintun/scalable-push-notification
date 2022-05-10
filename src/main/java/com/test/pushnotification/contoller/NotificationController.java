aa6735de392f1883bbd20e2670f291dd699b5750
Chan Nyein Tun <chanyeintun@gmail.com>
2022-05-10 13:52:31 +0630

    REFACTOR

--- src/main/java/com/test/pushnotification/contoller/NotificationController.java
+++ src/main/java/com/test/pushnotification/contoller/NotificationController.java
@@ -3,6 +3,8 @@ package com.test.pushnotification.contoller;
 
 import com.test.pushnotification.model.Notification;
 import com.test.pushnotification.payload.NotificationRequest;
+import com.test.pushnotification.payload.RedisNotificationPayload;
+import com.test.pushnotification.redis.RedisMessagePublisher;
 import com.test.pushnotification.service.EmitterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +23,15 @@ public class NotificationController {
     @Autowired
     private EmitterService emitterService;
 
+    @Autowired
+    private RedisMessagePublisher redisMessagePublisher;
+
     @GetMapping("/subscription")
     public SseEmitter subscribe(@RequestParam("uniqueToken") String uniqueToken) {
         log.info("subscribing...");
-        long tokenTimeOut=24 * 60 * 60 * 1000l;
+        long tokenTimeOut = 24 * 60 * 60 * 1000l;
         SseEmitter sseEmitter = new SseEmitter(tokenTimeOut);
-        emitterService.addEmitter(uniqueToken,sseEmitter);
+        emitterService.addEmitter(uniqueToken, sseEmitter);
 
         log.info("subscribed");
         return sseEmitter;
@@ -34,16 +39,18 @@ public class NotificationController {
 
     @PostMapping("/notification/{uniqueToken}")
     public ResponseEntity<?> send(@PathVariable String uniqueToken, @RequestBody NotificationRequest request) {
+        // ...
         // you can save and retrieve uniqueToken of the user from DB, this is just demo
-        Map<String,Object> payload=new HashMap<>();
-        payload.put("myValue",request.getMessage());
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("myValue", request.getMessage());
         Notification notification = Notification
                 .builder()
                 .from(request.getFrom())
                 .payload(payload)
                 .build();
 
-        emitterService.pushNotification(uniqueToken,notification);
+        redisMessagePublisher.publish(
+                new RedisNotificationPayload(uniqueToken, request.getFrom(), request.getMessage()));
         return ResponseEntity.ok().body("message pushed to uniqueToken " + uniqueToken);
     }
 }
